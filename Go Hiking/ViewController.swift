e08bba623e0c19fc0230ff3e0053c5d7c9e7462c
邱博晟 <ChiouBo@qiuboshengdeMacBook-Pro.local>
2020-01-26 17:21:21 +0800

    Import Firebase

---
+++ Go Hiking/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Go Hiking
+//
+//  Created by 邱博晟 on 2020/1/24.
+//  Copyright © 2020 Chioubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
